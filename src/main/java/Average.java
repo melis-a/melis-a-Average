07be1b2359a0773278b31a633f739718a2aa2873
Melisa Rampersaud <rampersaudmelisa@gmail.com>
2023-03-23 17:19:30 +0000

    avg

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,17 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        int i = 0;
+        double d = (double) i;
+        double sum = 0;
+
+        while(i < nums.length){
+            sum += nums[i];
+            i++;
+        }
+
+        double avg = (sum/ nums.length);
+
+        return avg;
     }
 }
